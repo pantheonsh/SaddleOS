00ac1f35a6be59f93d4363a6d09d06c182d78e99
Pantheon <pantheonshell@yahoo.com>
2018-09-17 14:44:08 -0300

    Interface desktop básica, nada faz ainda

--- root/start.js
+++ root/start.js
@@ -1,6 +1,10 @@
 const electron = require("electron");
+const path = require("path");
 const app = electron.app;
 
+let window;
+
 app.on("ready", () => {
-    
+    window = new electron.BrowserWindow({ width: 1280, height: 720, autoHideMenuBar: true, backgroundColor: "#000" });
+    window.loadFile(path.join(__dirname, "./interfaces/desktop.html"));
 });
